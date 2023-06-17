1d1f90887599d01d3c8e0823c434be214922ec5d
MohamedEmad <aleslam12345@gmail.com>
2023-06-17 06:30:19 +0300

    create ui

---
+++ app/src/main/java/com/example/postapost/presentaion/fragments/PostFragment.kt
@@ -0,0 +1,20 @@
+package com.example.postapost.presentaion.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.Fragment
+import com.example.postapost.R
+
+class PostFragment : DialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_post, container, false)
+    }
+
+}
