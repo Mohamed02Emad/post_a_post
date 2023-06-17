baea7195164779c148473ae0874e607792c16391
MohamedEmad <aleslam12345@gmail.com>
2023-06-17 06:44:01 +0300

    create posts adapter

--- app/src/main/java/com/example/postapost/presentaion/MainActivity.kt
+++ app/src/main/java/com/example/postapost/presentaion/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.postapost
+package com.example.postapost.presentaion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.postapost.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/postapost/presentaion/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/postapost/presentaion/fragments/HomeFragment.kt
@@ -5,16 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.postapost.R
+import com.example.postapost.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
 
+    private lateinit var binding: FragmentHomeBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+        return binding.root
     }
+
+
 }
